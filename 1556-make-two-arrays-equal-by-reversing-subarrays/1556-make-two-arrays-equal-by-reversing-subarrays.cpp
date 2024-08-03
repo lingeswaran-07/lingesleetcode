c3134c75d68fe13f5c945ae1743dd7ae87428bbe
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2024-08-03 07:15:05 +0530

    Time: 14 ms (24.92%), Space: 17.6 MB (42.71%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -4,14 +4,12 @@ public:
         sort(arr.begin(),arr.end());
         sort(target.begin(),target.end());
         for(int i=0;i<target.size();i++){
-            for(int j=0;j<arr.size();j++){
                 if(target[i]==arr[i]){
                     continue;
                 }
                 else{
                     return false;
                 }
-            }
         }
         return true;
         
