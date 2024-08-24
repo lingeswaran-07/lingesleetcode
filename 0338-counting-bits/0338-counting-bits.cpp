7ed6bbb59b427c6549c98f3512ac68fe0f596fe2
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2024-08-24 11:22:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (85.64%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>vec;
-        for(int i=0;i<=n;i++){
-            int cnt=__builtin_popcount(i);
-            vec.push_back(cnt);
+        vector<int>vec(n+1);
+        vec[0]=0;
+        for(int i=1;i<=n;i++){
+           vec[i]= vec[i>>1]+(i&1);
         }
         return vec;
     }
