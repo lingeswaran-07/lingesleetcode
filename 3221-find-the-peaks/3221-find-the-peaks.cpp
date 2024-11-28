1ee2d02d02b8efbf42f46698a7fe7edf00902065
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2024-11-28 19:00:41 +0530

    Time: 0 ms (100%), Space: 24.5 MB (27.92%) - LeetHub

---
+++ 3221-find-the-peaks/3221-find-the-peaks.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        int n=mountain.size();
+        vector<int>vec;
+        for(int i=1;i<n-1;i++){
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]){
+                      vec.push_back(i);
+            }
+        }
+
+        return vec;
+    }
+};
