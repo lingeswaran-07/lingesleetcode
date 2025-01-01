f60276b3b443ec10f81d4756f8cdc9d40273cfad
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-01-01 13:21:02 +0530

    Time: 1 ms (15.07%), Space: 9.1 MB (34.56%) - LeetHub

---
+++ 1156-occurrences-after-bigram/1156-occurrences-after-bigram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        int n=text.size();
+        stringstream ss(text);
+        string a;
+        vector<string>vec;
+        while(ss>>a){
+           vec.push_back(a);
+        }
+        vector<string>ans;
+        for (int i=0;i<vec.size()-2;i++){
+            if(vec[i]==first && vec[i+1]==second){
+                     ans.push_back(vec[i+2]);
+            }
+
+        } 
+        return ans;
+    }
+};
