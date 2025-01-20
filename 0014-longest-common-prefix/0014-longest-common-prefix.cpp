a0555ecf7e3deb7a1567639d09c5fa63fa05b247
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-01-20 15:10:28 +0530

    Time: 0 ms (100%), Space: 12 MB (35.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,7 +7,7 @@ public:
         int n=strs.size();
         string f=strs[0],l=strs[n-1];
         for(int i=0;i<min(f.size(),l.size());i++){
-            if(f[i]!=l[i]) return ans;
+            if(f[i]!=l[i]) break;
             ans+=f[i];
         }
      return ans;
