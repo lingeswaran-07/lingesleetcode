14e84f1090932b9e235fc680cba0551204de02b2
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-04-02 14:00:22 +0530

    Time: 0 ms (100%), Space: 14.6 MB (99.29%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -4,16 +4,19 @@ public:
         int cnt=1;
         int maxcnt=1;
         for(int i=1;i<nums.size();i++){
-            if(nums[i-1]<nums[i]){
-                cnt++;
-                if(maxcnt<cnt){
-                maxcnt=cnt;
-                }
+                     if(nums[i-1]<nums[i]){
+                          cnt++;
+                          maxcnt=max(maxcnt,cnt);
+                     } 
+                     else{
+                          maxcnt=max(maxcnt,cnt);
+                          cnt=1;
+                     }
+                   
             }
-            else{
-                cnt=1;
-            }
-        }
+           
+          
+        
         return  maxcnt;
         
     }
