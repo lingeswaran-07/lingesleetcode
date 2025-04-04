d12d40c4e457eb9c133fb1dd30161f03bc48fc04
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-04-04 22:25:51 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.37%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,25 @@
 class Solution {
-    void perm(vector<int>&per,vector<vector<int>>&ans,vector<int>&nums,int st ){
-        if(st==nums.size()){
-                  ans.push_back(nums);
-                  return;
-        }
+    // void perm(vector<int>&per,vector<vector<int>>&ans,vector<int>&nums,int st ){
+    //     if(st==nums.size()){
+    //               ans.push_back(nums);
+    //               return;
+    //     }
 
-        for(int i=st;i<nums.size();i++){
-            swap(nums[st],nums[i]);
-            perm(per,ans,nums,st+1);
-            swap(nums[st],nums[i]);
-        }
-    }
+    //     for(int i=st;i<nums.size();i++){
+    //         swap(nums[st],nums[i]);
+    //         perm(per,ans,nums,st+1);
+    //         swap(nums[st],nums[i]);
+    //     }
+    // }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        
-       vector<int>per;
-       vector<vector<int>>ans;
-       perm(per,ans,nums,0);
-       return ans;
+      sort(nums.begin(),nums.end());
+      vector<vector<int>>res;
+      do{
+        res.push_back(nums);
+      }
+      while(next_permutation(nums.begin(),nums.end()));
+    
+     return res;
     }
 };
