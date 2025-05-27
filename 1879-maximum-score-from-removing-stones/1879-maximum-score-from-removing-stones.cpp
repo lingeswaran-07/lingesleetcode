957ddc35b7f560347ba97350eb3256454a268b2e
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-05-27 11:17:25 +0530

    Time: 0 ms (100%), Space: 8 MB (49.03%) - LeetHub

---
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        if(a+b<c) return a+b;
+        if(b+c<a) return b+c;
+        if(a+c<b) return a+c;
+        return (a+b+c)/2;
+    }
+};
