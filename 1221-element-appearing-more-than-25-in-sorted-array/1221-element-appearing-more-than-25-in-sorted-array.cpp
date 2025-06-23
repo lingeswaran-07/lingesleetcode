c2ffa76c79c67a4d3312643ea228ba91ea400bc6
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-06-23 15:27:25 +0530

    Time: 4 ms (24.05%), Space: 17.3 MB (32.11%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.cpp
@@ -5,17 +5,12 @@ public:
         map<int,int>mp;
         for(int num :arr){
             mp[num]++;
-        }
-        int maxi=-1;
-        int ans=-1;
-
-        for(auto &it:mp){
-            if(maxi<it.second){
-                maxi=it.second;
-                ans=it.first;
+            if(mp[num]>n/4){
+                return num;
             }
         }
-        return ans;
+        
+        return -1;
         
     }
 };
