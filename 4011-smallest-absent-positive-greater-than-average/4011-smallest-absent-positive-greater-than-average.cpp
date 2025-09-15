f1824cefbb780642adc19a3c3fae349c9a4cc374
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-09-15 18:32:02 +0530

    Time: 32 ms (8.33%), Space: 92.6 MB (25%) - LeetHub

---
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        int n=nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int avg=sum/n;
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        int maxi=*max_element(nums.begin(),nums.end());
+        int ans=1;
+        if(avg<0){
+            for(int i=1;i<=maxi+1;i++){
+                if(i>avg && (mp.find(i)==mp.end())){
+                    ans=i;
+                    break;
+                }
+            }
+        }
+        else{
+        for(int i=1;i<=maxi+1;i++){
+            if(i>avg && (mp.find(i)==mp.end())){
+                ans=i;
+                break;
+            }
+        }
+        }
+        return ans;
+    }
+};
