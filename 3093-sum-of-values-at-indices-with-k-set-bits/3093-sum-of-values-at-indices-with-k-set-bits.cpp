32844cde186348c1e9100bb77747594f183e77ef
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-09-18 00:03:05 +0530

    Time: 0 ms (100%), Space: 26 MB (17.19%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -3,9 +3,14 @@ public:
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            int cnt=__builtin_popcount(i);
-            cout<<cnt<<endl;
-            if(cnt==k){
+            // int cnt=__builtin_popcount(i);
+            int a=i;
+            int c=0;
+            while(a!=0){
+                if((a&1)==1) c++;
+                 a>>=1;
+            }
+            if(c==k){
                 sum+=nums[i];
             }
         }
