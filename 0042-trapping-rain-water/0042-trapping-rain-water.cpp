5226c9b236070eda81c24b270f93601982c2effa
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-10-04 13:41:14 +0530

    Time: 0 ms (100%), Space: 26.2 MB (49.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,27 +4,30 @@ public:
         int n=height.size();
         int l=0;
         int r=n-1;
-        int lmax=0,rmax=0,tot=0;
+        int lmax=0;
+        int rmax=0;
+        int tot=0;
         while(l<r){
-                if(height[l]<=height[r] ){
-                    if(lmax>height[l]){
-                          tot+=lmax-height[l];
-                    }
-                    else{
-                        lmax=max(height[l],lmax);
-                    }
-                       l++;
+            if(height[l]<=height[r]){
+                if(lmax>height[l]){
+                    tot+=(lmax-height[l]);
+                    
                 }
                 else{
-                      if(rmax>height[r]){
-                          tot+=rmax-height[r];
-                    }
-                    else{
-                        rmax=max(height[r],rmax);
-                    }
-                       r--;
+                    lmax=max(lmax,height[l]);
                 }
-               
+                l++;
+            }
+            else{
+                if(rmax>height[r]){
+                     tot+=(rmax-height[r]);
+                    
+                }
+                else{
+                     rmax=max(rmax,height[r]);
+                }
+                 r--;
+            }
         }
         return tot;
     }
