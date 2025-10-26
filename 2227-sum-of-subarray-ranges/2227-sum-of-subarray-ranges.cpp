9108c5b40aebd185fd2524951fdbd694a73a6bb4
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-10-26 19:02:36 +0530

    Time: 23 ms (8.08%), Space: 14 MB (90.12%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,77 +1,18 @@
 class Solution {
-     long long sumSubarrayMins(vector<int>& arr) {
-      int n=arr.size();
-      vector<long long>pse(n);
-      vector<long long>nse(n);
-      stack<int>st1;
-      stack<int>st2;
-      for(int  i=0;i<n;i++){
-        while(!st1.empty() && arr[st1.top()]>arr[i]){
-            st1.pop();
-        }
-        pse[i]=st1.empty()?-1:st1.top();
-        st1.push(i);
-      }
-      
-
-      for(int  i=n-1;i>=0;i--){
-        while(!st2.empty() && arr[st2.top()]>=arr[i]){
-            st2.pop();
-        }
-        nse[i]=st2.empty()?n:st2.top();
-        st2.push(i);
-      }
-      long long  tot=0;
-
-      for(int i=0;i<n;i++){
-        long long  l=i-pse[i];
-        long long r=nse[i]-i;
-      tot = (tot + (l * r  * arr[i] *1LL) );
-      }
-
-      return tot;
-    }
-
-      long long  sumSubarraymaxima(vector<int>& arr) {
-      int n=arr.size();
-      vector<long long>pse(n);
-      vector<long long>nse(n);
-      stack<int>st1;
-      stack<int>st2;
-      for(int  i=0;i<n;i++){
-        while(!st1.empty() && arr[st1.top()]<arr[i]){
-            st1.pop();
-        }
-        pse[i]=st1.empty()?-1:st1.top();
-        st1.push(i);
-      }
-      
-
-      for(int  i=n-1;i>=0;i--){
-        while(!st2.empty() && arr[st2.top()]<=arr[i]){
-            st2.pop();
-        }
-        nse[i]=st2.empty()?n:st2.top();
-        st2.push(i);
-      }
-
-      long long  tot=0;
-
-      for(int i=0;i<n;i++){
-        long long  l=i-pse[i];
-        long long r=nse[i]-i;
-       tot = (tot + (l * r  * arr[i] *1LL) );
-      }
-
-      return tot;
-
-
-       
-    }
 public:
     long long subArrayRanges(vector<int>& nums) {
-           return sumSubarraymaxima(nums) - sumSubarrayMins(nums);
-
-        
+        long long sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int maxi=INT_MIN;
+            int mini=INT_MAX;
+            for(int j=i;j<n;j++){
+                maxi=max(maxi,nums[j]);
+                mini=min(mini,nums[j]);
+                sum+=(maxi-mini);
+            }
+            
+        }
+        return sum;
     }
 };
