8f351c60b930480f3c867ccd41e1b732d9b5d8a5
lingeswaran-07 <136102568+lingeswaran-07@users.noreply.github.com>
2025-12-28 20:38:12 +0530

    Time: 986 ms (5.02%), Space: 22.1 MB (99.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+
+   bool isanagram(string &s,string &l){
+    if(s.length()!=l.length()) return false;
+    int fre[26]={0};
+    for(char ch:s) fre[ch-'a']++;
+    for(char ch:l) fre[ch-'a']--;
+
+    for(int i=0;i<26;i++){
+        if(fre[i]!=0) return false;
+    }
+    return true;
+   }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
-        unordered_map<string,vector<string>>mp;
+        
+        for(string word:strs){
+            bool flag=false;
+            for(auto &g:ans){
+                if(isanagram(g[0],word)){
+                    g.push_back(word);
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag){
+                ans.push_back({word});
+            }
 
-        for(string s:strs){
-            string ss=s;
-            sort(ss.begin(),ss.end());
-            mp[ss].push_back(s);
-        }
-
-        for(auto &it :mp){
-            ans.push_back(it.second);
         }
         return ans;
     }
