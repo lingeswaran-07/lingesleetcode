a6fab15ce946e8e046d65370ba76b437f8213f55
Lingeswaran V <136102568+lingeswaran-07@users.noreply.github.com>
2026-09-06 23:51:54 +0530

    Time: 0 ms (100%), Space: 12.7 MB (49.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-         int n=nums.size();
-        
-          if (n == 1) return 0;
-         if (nums[0] > nums[1]) return 0;
-        if (nums[n - 1] > nums[n - 2]) return n - 1;
-         int l=1,h=n-2;
-         while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid+1]<nums[mid]){
-                return mid;
+        int n=nums.size();
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                return i;
             }
-            if(nums[mid]>nums[mid-1]){
-                l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }    
-
-         }
-         return -1;
+        }
+        return -1;
     }
 };
